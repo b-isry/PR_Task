2658cb0cdc471473b0f36747b1065f0dc71ec6c2
b-isry <bisratt1995@gmail.com>
2025-03-13 06:18:49 +0300

    add test cases to Test.py

---
+++ Test.py
@@ -0,0 +1,22 @@
+import unittest
+from Calculator import Calculator 
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(5, 3), 8)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(10, 4), 6)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+
+
+if __name__ == "__main__":
+    unittest.main()
